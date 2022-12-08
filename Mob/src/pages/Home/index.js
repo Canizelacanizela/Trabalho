bc02e9ccbbfc26847ea33df1e4c05b0f14430005
Canizelacanizela <98489143+Canizelacanizela@users.noreply.github.com>
2022-12-08 10:29:28 -0300

    Add files via upload

---
+++ Mob/src/pages/Home/index.js
@@ -0,0 +1,17 @@
+import {useState} from "react"
+import { View, Image,StyleSheet, TouchableOpacity, TextInput, Text} from 'react-native';
+import Button from '../../components/button/index';
+
+import styles from './style';
+
+export default function Home() {
+
+    return(
+        <View style={styles.container}>
+            
+            
+            
+        </View>
+    )
+}
+
